b564ecdc48e370c022790457e8127d0f340f1fe8
jonathan <jonodrew@users.noreply.github.com>
2021-06-27 17:37:26 +0100

    Tidy up imports

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
-from flask import Flask, request, make_response, render_template, url_for, session, abort
+from flask import Flask, request, make_response, render_template, session, abort
 from flask import redirect as flask_redirect
 import tweepy
 from process_interface import ProcessInterfaceFactory
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
-app.queue_interface = ProcessInterfaceFactory.create_interface(os.environ.get('QUEUE_TOOL'))
+app.queue_interface = ProcessInterfaceFactory.create_interface(
+    os.environ.get("QUEUE_TOOL")
+)
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/follow/<list_id>', methods=["GET", "POST"])
+@app.route("/follow/<list_id>", methods=["GET", "POST"])
 def follow_list(list_id: int):
     """
     Follow a specific list. The GET request returns a form, asking for the user's Twitter username, and sets a cookie
@@ -24,37 +26,51 @@ def follow_list(list_id: int):
     :return:
     """
     if request.method == "POST":
-        session['user_id'] = request.form.get('twitter-username')
-        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'),
-                                   callback="http://127.0.0.1:5000/redirect")
+        session["user_id"] = request.form.get("twitter-username")
+        auth = tweepy.OAuthHandler(
+            os.environ.get("consumer_key"),
+            os.environ.get("consumer_secret"),
+            callback="http://127.0.0.1:5000/redirect",
+        )
         auth_url = auth.get_authorization_url()
-        session['request_token'] = auth.request_token.get('oauth_token', None)
+        session["request_token"] = auth.request_token.get("oauth_token", None)
         return flask_redirect(location=auth_url)
     elif request.method == "GET":
-        response = make_response(render_template('follow-list.html'))
-        session['list_id'] = list_id
+        response = make_response(render_template("follow-list.html"))
+        session["list_id"] = list_id
         return response
 
 
-@app.route('/redirect')
+@app.route("/redirect")
 def redirect():
-    if session.get('list_id', None) is None:
+    if session.get("list_id", None) is None:
         abort(404)  # return 404
     else:
-        request_token = session['request_token']
-        del session['request_token']
+        request_token = session["request_token"]
+        del session["request_token"]
 
-        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'),
-                                   callback="http://127.0.0.1:5000/redirect")
-        verifier = request.args.get('oauth_verifier')
+        auth = tweepy.OAuthHandler(
+            os.environ.get("consumer_key"),
+            os.environ.get("consumer_secret"),
+            callback="http://127.0.0.1:5000/redirect",
+        )
+        verifier = request.args.get("oauth_verifier")
 
-        auth.request_token = {'oauth_token': request_token, 'oauth_token_secret': verifier}
+        auth.request_token = {
+            "oauth_token": request_token,
+            "oauth_token_secret": verifier,
+        }
         auth.get_access_token(verifier)
 
-        app.queue_interface.process(session['user_id'], session['list_id'], auth.access_token, auth.access_token_secret)
+        app.queue_interface.process(
+            session["user_id"],
+            session["list_id"],
+            auth.access_token,
+            auth.access_token_secret,
+        )
 
         return "We'll now process the request"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
