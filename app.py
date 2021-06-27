30b2dfcb216c9ea075911fa25e2e14fd962fb082
jonathan <jonathan.drew.kerr@gmail.com>
2021-06-27 15:09:52 +0100

    Implement very basic frontend
    Significant changes here. It's not good practice to put them all in one commit, but it's wet and horrible and I feel quite rotten. So!
    
    There's now a process_interface.py file, which is where multiple approaches to implementing the ProcessInterface live. It includes a factory, which allows you to run the frontend reasonably painlessly locally and test everything looks normal.
    
    I've added a bit of a frontend, though it's all nicked from Bootstrap and definitely needs some polish from someone who does frontend type things.

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask, request, make_response, render_template, url_for, session
+from flask import Flask, request, make_response, render_template, url_for, session, abort
+from flask import redirect as flask_redirect
 import tweepy
-import boto3
-import json
+from process_interface import ProcessInterfaceFactory
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
+app.queue_interface = ProcessInterfaceFactory.create_interface(os.environ.get('QUEUE_TOOL'))
 
 
 @app.route('/', methods=["GET"])
 def index():
-    response = make_response(render_template('index.html'))
-    return
+    return render_template('index.html')
 
 
 @app.route('/follow/<list_id>', methods=["GET", "POST"])
@@ -24,39 +24,36 @@ def follow_list(list_id: int):
     :return:
     """
     if request.method == "POST":
-        pass
-        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'), url_for('redirect'))
-        session['request_token'] = auth.request_token
-        return redirect(auth.get_authorization_url())
+        session['user_id'] = request.form.get('twitter-username')
+        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'),
+                                   callback="http://127.0.0.1:5000/redirect")
+        auth_url = auth.get_authorization_url()
+        session['request_token'] = auth.request_token.get('oauth_token', None)
+        return flask_redirect(location=auth_url)
     elif request.method == "GET":
-        response = make_response(render_template('follow.html'))
+        response = make_response(render_template('follow-list.html'))
         session['list_id'] = list_id
         return response
 
 
 @app.route('/redirect')
 def redirect():
-    if request.cookies.get('list_id', None) is None:
-        pass  # return 404
+    if session.get('list_id', None) is None:
+        abort(404)  # return 404
     else:
         request_token = session['request_token']
         del session['request_token']
 
-        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'), url_for('redirect'))
-        auth.request_token = request_token
+        auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'),
+                                   callback="http://127.0.0.1:5000/redirect")
         verifier = request.args.get('oauth_verifier')
+
+        auth.request_token = {'oauth_token': request_token, 'oauth_token_secret': verifier}
         auth.get_access_token(verifier)
 
-        package = {
-            'access_token': auth.access_token,
-            'access_token_secret': auth.access_token_secret,
-            'user_id': session['user_id'],
-            'list_id': session['list_id']
-        }
+        app.queue_interface.process(session['user_id'], session['list_id'], auth.access_token, auth.access_token_secret)
 
-        sqs_resource = boto3.resource('sqs')
-        queue = sqs_resource.get_queue_by_name(QueueName='process')
-        queue.send_message(MessageBody=json.dumps(package))
+        return "We'll now process the request"
 
 
 if __name__ == '__main__':
--- process_interface.py
+++ process_interface.py
@@ -1,19 +1,73 @@
 import abc
+import json
+import os
+import re
+import time
 from abc import ABC
+
+import boto3
 
 
 class ProcessInterface(ABC):
 
+    @staticmethod
+    def prepare_package(username: str, list_id: str, access_token: str, access_token_secret: str):
+        package = {
+            'access_token': access_token,
+            'access_token_secret': access_token_secret,
+            'user_id': username,
+            'list_id': list_id
+        }
+        return package
+
     @abc.abstractmethod
-    def process(self, username: str, list_id: str):
+    def process(self, username: str, list_id: str, access_token: str, access_token_secret: str):
         pass
 
     @property
     @abc.abstractmethod
-    def _db(self):
+    def db(self):
         pass
 
 
 class AWSProcessInterface(ProcessInterface):
     def __init__(self, db_url, ):
+        self.db = db_url
+
+    def process(self, username: str, list_id: str, access_token: str, access_token_secret: str):
+        package = self.prepare_package(username, list_id, access_token, access_token_secret)
+
+        sqs_resource = boto3.resource('sqs')
+        queue = sqs_resource.get_queue_by_name(QueueName='process')
+        response = queue.send_message(MessageBody=json.dumps(package))
+        return response
+
+    @property
+    def db(self):
+        return self.db
+
+    @db.setter
+    def db(self, url):
+        if re.match(r'aws', url):
+            self.db = url
+        else:
+            raise Exception
+
+
+class TestProcessAPI(ProcessInterface):
+    def __init__(self):
         pass
+
+    def process(self, username: str, list_id: str, access_token: str, access_token_secret: str):
+        time.sleep(4)
+        return 200
+
+    def db(self):
+        return None
+
+
+class ProcessInterfaceFactory:
+    @classmethod
+    def create_interface(cls, queue_tool):
+        if queue_tool is None:
+            return TestProcessAPI()
